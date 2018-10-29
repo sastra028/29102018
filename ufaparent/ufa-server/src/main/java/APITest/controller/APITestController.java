98f08dcb75ce1bb204ce4a38d5c89f66223eb74d
Top-PC <sastra.028@gmail.com>
2018-10-30 01:44:25 +0700

    Add angular project

--- ufaparent/ufa-server/src/main/java/APITest/controller/APITestController.java
+++ ufaparent/ufa-server/src/main/java/APITest/controller/APITestController.java
@@ -3,10 +3,15 @@ package APITest.controller;
 
 import APITest.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@Controller
+//@RequestMapping("/home")
 public class APITestController {
 
     @Autowired
@@ -15,4 +20,8 @@ public class APITestController {
     public String helloApi(){
         return apiService.getDataFromTableA1All();
     }
+//@GetMapping
+//public String home(Model model) {
+//    return "forward:/index.html";
+//}
 }
